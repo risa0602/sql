9077bc944111b7d6f3695c4ad98464abe94f7030
risa <zenzaiumum@gmail.com>
2023-07-03 13:39:16 +0900

    firstcommit

---
+++ Lesson3kai.sql
@@ -0,0 +1,95 @@
+SELECT
+title AS タイトル,
+price AS 価格
+FROM books;
+
+SELECT 
+DISTINCT
+category
+FROM books;
+
+SELECT
+price,
+price+100,
+'Hello'
+FROM books;
+
+SELECT floor(120 * 1.08);
+
+SELECT format(12345678,0);
+
+SELECT
+title AS タイトル ,
+format(price,0) AS 価格
+FROM books;
+
+SELECT 
+title AS 書名,
+category AS 分類,
+CASE category
+WHEN '雑誌' THEN '1F'
+WHEN '漫画' THEN '1F'
+ELSE '2F'
+END AS 階
+FROM books;
+
+SELECT
+title AS 書名 ,
+price AS 価格 ,
+CASE
+WHEN price <500 THEN 'えんぴつ'
+WHEN price <1000 THEN 'キーホルダー'
+WHEN price <3000 THEN 'ポスター'
+ELSE'腕時計'
+END AS プレゼント
+FROM books;
+
+SELECT
+title,char_length(title)
+FROM books;
+
+INSERT INTO books
+(title,price,updated)
+VALUES
+('SQL入門',2800,curdate());
+
+SELECT now();
+
+SELECT 
+concat(title,':',price,'円')
+FROM books;
+
+SELECT 
+title,
+coalesce(updated,'更新日未記入')
+FROM books;
+
+SELECT 
+title,price
+FROM books
+WHERE
+price =
+(SELECT max(price) FROM books);
+
+# 内部結合
+SELECT * FROM cds
+JOIN categories
+ON cds.cat_id=categories.id;
+
+# 左結合
+SELECT * FROM cds
+LEFT JOIN categories
+ON cds.cat_id=categories.id;
+
+# 右結合
+SELECT * FROM cds
+RIGHT JOIN categories
+ON cds.cat_id=categories.id;
+
+SELECT cds.title,categories.category,cds.price FROM cds
+LEFT JOIN categories
+ON cds.cat_id=categories.id;
+
+SELECT cd.title,cat.category,cd.price FROM cds AS cd
+LEFT JOIN categories AS cat
+ON cd.cat_id=cat.id;
---
+++ lesson4.sql
@@ -0,0 +1,7 @@
+INSERT INTO emps(name,age,dep_id)
+VALUES('山田',23,1);
+
+SELECT * FROM sales 
+
+UPDATE sales SET  emps = emps + 1;
+WHERE id = 5;
